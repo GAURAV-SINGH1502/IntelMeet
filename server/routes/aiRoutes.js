27a35945d993b9a4c88ff980a2c148404b844ab7
Gaurav Singh <gauravsingh15022005@gmail.com>
2026-06-09 11:33:07 +0530

    Fixed Gemini model

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -18,7 +18,7 @@ router.post(
 
       const model =
         genAI.getGenerativeModel({
-          model: "gemini-1.5-flash",
+          model: "gemini-2.0-flash",
         });
 
       const prompt = `
